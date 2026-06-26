a14292b0c337de9ba1250727ce265cfc2f2d2fce
wadamec <wadamec@freeshell.de>
2026-06-26 21:48:07 +0200

    next

---
+++ arthos/arthos_code_lib.php
@@ -0,0 +1,256 @@
+<?php
+
+/******************************************************************************************
+ *                                                                                        *
+ ******************************************************************************************/
+function log_message ($message, $level) 
+{
+  // Log-Datei im selben Verzeichnis (oder einem dedizierten /logs/ Ordner)
+  $logFile = __DIR__ . '/person_id.log';
+    
+  $timestamp = date ('Y-m-d H:i:s');
+    
+  // Wenn Daten (Arrays/Objekte) übergeben werden, lesbar formatieren
+  //$dataString = '';
+  //if ($data !== null) {
+  //  $dataString = " | Data: " . print_r($data, true);
+  //   // Zeilenumbrüche aus print_r für ein sauberes Einzeilen-Log-Format bereinigen
+  //  $dataString = str_replace(array("\r", "\n"), " ", $dataString);
+  //}
+    
+  $logEntry = "[$timestamp] [$level] $message\n";
+    
+  // Sicherstellen, dass in die Datei geschrieben werden darf
+  file_put_contents ($logFile, $logEntry, FILE_APPEND);
+}
+
+/******************************************************************************************
+ * Return: true | false.                                                                  *                                                                       *
+ ******************************************************************************************/
+function str_check_hash ($str_line)
+{
+  $int_pos = 0;
+  
+  // evidence#value-start (8).
+  $int_pos = strpos ($str_line, "#value-start"); // ret.: int/false.
+  if ($int_pos === false) {
+	return false;
+  }
+  
+  return true;  
+}
+
+/******************************************************************************************
+ * Return: string | false.                                                                *                                                                       *
+ ******************************************************************************************/
+function get_hash_name ($str_line)
+{ 
+  $bool_result = false;
+  $int_pos     = 0;
+  $str_result  = "";
+  
+  $bool_result = str_check_hash ($str_line);
+  if ($bool_result === false) {
+	log_message ("get_hash_name.check_hash", "error");
+	return false;
+  }   
+ 
+  // evidence#value-start (8).
+  $int_pos    = strpos ($str_line, "#value-start"); // ret.: int/false.
+  $str_result = substr ($str_line, 0, $int_pos);  
+
+  log_message ("hash name", "info");
+  log_message ($str_result, "info");
+
+  return $str_result;
+}
+
+/******************************************************************************************
+ * Return: string | false.                                                                *                                                                       *
+ ******************************************************************************************/
+function get_hash_value ($handle)
+{ 
+  $bool_start     = true;
+  $int_pos        = 0;
+  $str_hash_value = "";
+  $str_line       = "";
+  
+  log_message ("get_hash_value.start", "info");
+  
+  while (true) {
+    $str_line = fgets ($handle); // ret.: str./false.
+	if ($str_line === false) {
+	  log_message ("get_hash_value.fgets", "error");
+      return false;			
+	}	
+	
+	#value-end  
+	$int_pos = strpos ($str_line, "#value-end"); // ret.: int/false.
+	if ($int_pos === false) {
+	  log_message ("hash-data", "info");	
+	  log_message ($str_line, "info");	
+	  
+	  $str_line = trim ($str_line);
+	  if ($bool_start === false) {
+		$str_hash_value .= " ";
+	  }
+	  else {
+		$bool_start = false;
+	  }
+	  $str_hash_value .= $str_line;
+	}
+    else {
+      log_message ("hash_value", "info");
+	  log_message ($str_hash_value, "info");
+	  
+	  return $str_hash_value;	
+	}
+  } // while
+
+}
+
+/******************************************************************************************
+ *                                                                                        *                                                                       *
+ ******************************************************************************************/
+function query_str_get_value ($arr_query_str, $str_attr)
+{
+  $bool_result = false;
+  $str_return  = "";  
+   
+  $bool_result = array_key_exists ($str_attr, $arr_query_str);
+  
+  if ($bool_result === false) {
+    return false;
+  }
+  
+  $str_return = $arr_query_str [$str_attr];
+  
+  return $str_return;
+}
+
+/******************************************************************************************
+ *                                                                                        *                                                                                       *
+ ******************************************************************************************/
+function log_mapping ($mapping)
+{
+  $log_line = "";
+  
+  foreach ($mapping as $name => $value) {
+    $log_line .= "{$name}: {$value}\n";	  
+  }
+  log_message ($log_line, "info");
+}
+
+/******************************************************************************************
+ * Return: Mapping (Ass. Array) | false.                                                  *                                                                                       *
+ ******************************************************************************************/
+function create_mapping ($file_path)
+{
+  $arr_parts     = [];
+  $arr_ret_value = [];
+  $bool_result   = false;
+  $handle        = null;  
+  $int_dummy     = 0;
+  $int_pos       = 0;
+  $int_str_len   = 0;
+  $str_line      = "";
+  $str_name      = "";
+  $str_name_ext  = "";
+  $str_value     = "";
+  
+  $bool_result = file_exists ($file_path);
+  
+  if ($bool_result === false) {
+    log_message ("create_mapping.file_does_not_exist", "error");
+    return false;	
+  }	  
+  
+  $handle = fopen ($file_path, "r"); // ret.: resource/false.
+  
+  if ($handle === false) { 
+    log_message ("create_mapping.fopen", "error");
+    return false;	
+  }
+
+  // Der Header wird überlesen.
+  while (true) {
+    $str_line = fgets ($handle); // ret.: str./false.
+    if ($str_line === false) {
+      log_message ("create_mapping.header_fgets", "error");
+      return false;			
+    }	
+	
+    log_message ("create_mapping.header_line", "info");
+    log_message ($str_line, "info");
+	
+    $int_pos = strpos ($str_line, "#data-start"); // ret.: int/false.
+    if ($int_pos !== false) {
+      log_message ("Punkt erreicht", "info");
+      break;
+    }
+  }
+
+  log_message ("Daten.", "info");
+
+  // last_name|Falcone
+  // Die Name-Value-Paare werden nun verarbeitet.
+  while (true) {
+    $str_line = fgets ($handle);
+    if ($str_line === false) {
+      log_message ("create_mapping.data_fgets", "error");
+      return false;			
+    }
+    $str_line = rtrim ($str_line);
+    log_message ("Zeile.", "info");
+    log_message ($str_line, "info");
+
+    // Kommentare werden einfach überlesen.
+    $int_pos = strpos ($str_line, "#comment:"); // ret.: int/false.
+    if ($int_pos !== false) {
+      continue;	  	
+    }
+	
+    // Das ist die Abbruchsbedingung.
+    $int_pos = strpos ($str_line, "#data-end"); // ret.: int/false.
+    if ($int_pos !== false) {
+      log_message ("Datei-Ende erreicht", "info");		
+      break;	  	
+    }
+	
+    $bool_result = str_contains ($str_line, '|'); // ret.: bool.
+    if ($bool_result === true) {
+      log_message ("pipe verar.", "info");
+      $arr_parts    = explode ("|", $str_line, 2);
+      $str_name     = trim ($arr_parts [0]);	  
+      $str_name_ext = "{{" . $str_name . "}}";         	  
+      $str_value    = trim ($arr_parts [1]);	  	  
+      $arr_ret_value [$str_name_ext] = $str_value;
+	  
+      log_message ("Pipe: {$str_name} {$str_value}", "info");		
+    }
+    else {
+      // evidence#value-start
+      $bool_result = str_check_hash ($str_line); // ret.: bool.
+      if ($bool_result === true) {
+        log_message ("Hash start", "info");				  
+		
+        $str_name     = get_hash_name ($str_line);  
+        $str_name_ext = "{{" . $str_name . "}}";         	  
+	$str_value    = get_hash_value ($handle); // ret: str/false.
+		
+	if ($str_value === false) {
+	  log_message ("create_mapping.hash-value", "error");
+	  return false;
+	}
+		
+        $arr_ret_value [$str_name_ext] = $str_value;
+		
+	log_message ("Hash: {$str_name} {$str_value}", "info");		
+      }
+    }			
+	
+  } // while	  
+
+  return $arr_ret_value;
+}
+
--- arthos/persons/show_person.php
+++ arthos/persons/show_person.php
@@ -8,29 +8,42 @@
   $str_id = "";
   $str_lang = ""; 
 
-  require_once 'helper_fun.php';
+  require_once __DIR__ . "/../arthos_code_lib.php";
 
   log_message ("-----------------------", "info");
   log_message ("New call.",               "info");
 
+  $str_sapi_name = php_sapi_name();
+  if ($str_sapi_name === "cli") {
+    $_GET["id"] = "1.1";
+    $_GET["lang"] = "deu";
+  }
+
   $str_id = query_str_get_value ($_GET, "id"); // false/str.  
   if ($str_id === false) {
-    log_message ("main.query_str", "error");	
+    log_message ("main.query_str_get_value-id", "error");	
     exit;
   } 
 
   $str_lang = query_str_get_value ($_GET, "lang"); // false/str.
-  
-  log_message ("ID: {$str_id}", "info");
+  if ($str_lang === false) {
+    log_message ("main.query_str_get_value-lang", "error");
+    exit;
+  }  
+
+  log_message ("ID:   {$str_id}", "info");
+  log_message ("lang: {$str_lang}", "info");
+
   $str_id    = str_replace (".", "_", $str_id);  
-  $file_name = "person_" . $str_id . ".art";
+  $file_name = "person_" . $str_lang . "_" . $str_id . ".art";
   
+  log_message ($file_name, "info");
+
   $data_map = create_mapping ($file_name); 
   log_mapping ($data_map);  
-  <M-:>wq
   if ($data_map === false) {
-	log_message ("main.create_mapping", "error");	
-	exit;
+    log_message ("main.create_mapping", "error");	
+    exit;
   }	  
   
   log_message ("nach create_mapping", "info");
