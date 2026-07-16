f1a2e931f843229713ab51acdc18ba7adde2d671
Wolfgang Adamec <wolfgang.adamec@gmail.com>
2026-07-16 07:25:34 +0200

    next

--- arthos/arthos_code_lib.php
+++ arthos/arthos_code_lib.php
@@ -27,15 +27,49 @@ function log_message ($message, $level)
 /*******************************************************************************
  *                                                                             *
  *******************************************************************************/
+function find_entry ($arr_files, $int_name_len)
+{  
+  $char_char     = "";
+  $bool_digit    = false;
+  $str_ret_value = "";
+  $str_file_name = "";
+  
+  foreach ($arr_files as $str_path) {
+	log_message ("find_entry_file", "info");	
+	log_message ($str_path,		    "info");	
+	
+	// Ich bekomme einen ganzen Pfad zurueck, moechte aber nur den Dateinamen zurueckgeben.
+    $str_file_name = basename ($str_path);	
+    $char_char     = $str_file_name [$int_name_len];      	  
+	log_message ($int_name_len, "info");	
+	
+	$bool_digit = ctype_digit ($char_char);
+    if ($bool_digit === false) {
+      $str_ret_value = $str_file_name;
+      break;	  
+    }		
+  }	  
+  
+  return $str_ret_value;
+}
+
+/*******************************************************************************
+ *                                                                             *
+ *******************************************************************************/
 function get_full_file_name ($base_dir, $name_part)
 {
-  $arr_dateien = [];
-  $suchmuster  = "";
-  $int_count   = 0;
-  $file_name   = "";
-  $file_path   = "";
+  $arr_dateien   = [];
+  $suchmuster    = "";
+  $int_count     = 0;
+  $file_name     = "";
+  $file_path     = "";
+  $name_part_len = 0;
 
-  $suchmuster = $base_dir . DIRECTORY_SEPARATOR . $name_part . "*";
+  // lexeme_deu_1_3_1_actions_context_dialog.art, lexeme_deu_1_3_1_1_turm.
+  // lexeme_deu_1_3_1_
+
+  $name_part_len = strlen ($name_part);
+  $suchmuster    = $base_dir . DIRECTORY_SEPARATOR . $name_part . "*";
 
   $arr_dateien = glob ($suchmuster);
   if ($arr_dateien === false) {
@@ -43,13 +77,14 @@ function get_full_file_name ($base_dir, $name_part)
   }
 
   $int_count = count ($arr_dateien);
-  if ($int_count !== 1) {
-    return false;
+  if ($int_count === 1) {
+    $file_path = $arr_dateien [0];
+    // Ich bekomme einen ganzen Pfad zurueck, moechte aber nur den Dateinamen zurueckgeben.
+    $file_name = basename ($file_path);
   }
-
-  // Ich bekomme einen ganzen Pfad zurueck, moechte aber nur den Dateinamen zurueckgeben.
-  $file_path = $arr_dateien [0];
-  $file_name = basename ($file_path);
+  else {
+	$file_name = find_entry ($arr_dateien, $name_part_len);
+  }
 
   return $file_name;
 }
@@ -63,6 +98,22 @@ function str_check_hash ($str_line)
   
   // evidence#value-start (8).
   $int_pos = strpos ($str_line, "#value-start"); // ret.: int/false.
+  if ($int_pos === false) {
+    return false;
+  }
+  
+  return true;  
+}
+
+/*******************************************************************************
+ * Return: true | false.                                                       *                                                                       
+ *******************************************************************************/
+function str_check_array ($str_line)
+{
+  $int_pos = 0;
+  
+  // evidence#array-start (8).
+  $int_pos = strpos ($str_line, "#array-start"); // ret.: int/false.
   if ($int_pos === false) {
     return false;
   }
@@ -91,6 +142,28 @@ function get_hash_name ($str_line)
 
   log_message ("hash name", "info");
   log_message ($str_result, "info");
+
+  return $str_result;
+}
+
+/*******************************************************************************
+ * Return: string | false.                                                     *                                                                       
+ *******************************************************************************/
+function get_array_name ($str_line)
+{ 
+  $bool_result = false;
+  $int_pos     = 0;
+  $str_result  = "";
+  
+  $bool_result = str_check_array ($str_line);
+  if ($bool_result === false) {
+	log_message ("get_array_name.check_array", "error");
+	return false;
+  }   
+ 
+  // evidence#array-start (8).
+  $int_pos    = strpos ($str_line, "#array-start"); // ret.: int/false.
+  $str_result = substr ($str_line, 0, $int_pos);  
 
   return $str_result;
 }
@@ -140,6 +213,55 @@ function get_hash_value ($handle)
 }
 
 /*******************************************************************************
+ * Return: string | false.                                                     *                                                                       
+ *******************************************************************************/
+function get_array_value ($handle)
+{ 
+  $bool_start      = true;
+  $int_pos         = 0;
+  $str_array_value = "";
+  $str_line        = "";
+  $str_name        = "";
+  $str_id          = "";
+  $arr_parts       = [];
+  
+  log_message ("get_array_value.start", "info");
+  
+  while (true) {
+    $str_line = fgets ($handle); // ret.: str./false.
+	if ($str_line === false) {
+	  log_message ("get_array_value.fgets", "error");
+      return false;			
+	}	
+	
+	#array-end  
+	$int_pos = strpos ($str_line, "#array-end"); // ret.: int/false.
+	if ($int_pos === false) {
+	  log_message ("array-data", "info");	
+	  log_message ($str_line, "info");		  
+
+      $int_pos = strpos ($str_line, "|"); // ret.: int/false.
+      if ($int_pos !== false) {
+        log_message ("get_array_value.no-pipe", "error");
+        return false;
+	  }
+	  $arr_parts  = explode ("|", $str_line, 2);
+	  $str_name   = trim ($arr_parts [0]);	  
+      $str_id     = trim ($arr_parts [1]);	  	  
+      
+	  $str_array_value .= "<tr><td>" . $str_name . "</td><td class=\"id-col\">" . $str_id . "</td></tr>";
+	}
+    else {
+      log_message ("array_value", "info");
+	  log_message ($str_array_value, "info");
+	  
+	  return $str_array_value;	
+	}
+  } // while
+
+}
+
+/*******************************************************************************
  *                                                                             *                                                                       
  *******************************************************************************/
 function query_str_get_value ($arr_query_str, $str_attr)
@@ -176,20 +298,21 @@ function log_mapping ($mapping)
  *******************************************************************************/
 function prepare_card ($code, $base_dir)
 {
-  $str_id = "";
-  $str_lang = ""; 
-  $str_sapi_name = "";
+  $str_id         = "";
+  $str_lang       = ""; 
+  $str_format     = "";
+  $str_sapi_name  = "";
   $file_name_full = "";
   $file_name_part = "";
-  $html_template = "";
-  $output = "";
-  $template_name = "";
+  $html_template  = "";
+  $output         = "";
+  $template_name  = "";
 
   $str_sapi_name = php_sapi_name ();
   if ($str_sapi_name === "cli") {
     $_GET["id"]     = "1.1";
     $_GET["lang"]   = "deu";
-	$_GET["format"] = 
+	$_GET["format"] = "normal";
   }
 
   $str_id = query_str_get_value ($_GET, "id"); // false/str.  
@@ -204,11 +327,19 @@ function prepare_card ($code, $base_dir)
     exit;
   }  
 
-  log_message ("ID:   {$str_id}", "info");
-  log_message ("lang: {$str_lang}", "info");
+  $str_format = query_str_get_value ($_GET, "format"); // false/str.  
+  if ($str_format === false) {
+	$str_format = "normal";  
+  } 
+
+  log_message ("ID:     {$str_id}",     "info");
+  log_message ("lang:   {$str_lang}",   "info");
+  log_message ("format: {$str_format}", "info");
 
   $str_id         = str_replace (".", "_", $str_id);  
-  $file_name_part = $code . "_" . $str_lang . "_" . $str_id;
+  $file_name_part = $code . "_" . $str_lang . "_" . $str_id . "_";
+  
+  log_message ($file_name_part, "info");
   
   $file_name_full = get_full_file_name ($base_dir, $file_name_part);
   if ($file_name_full ===  false) {
@@ -216,7 +347,6 @@ function prepare_card ($code, $base_dir)
     exit;
   }
 
-  log_message ($file_name_part, "info");
   log_message ($file_name_full, "info");
 
   $data_map = create_mapping ($file_name_full); 
@@ -228,7 +358,21 @@ function prepare_card ($code, $base_dir)
   
   log_message ("nach create_mapping", "info");
 
-  $template_name = $code . "-template.html";
+  // lexeme-template.html, lexeme-short.html, lexeme-extended.html
+  switch ($str_format) {
+    case "normal": 
+      $template_name = $code . "-template.html";
+      break;
+    case "short":
+      $template_name = $code . "-short.html";
+      break;
+	case "ext":
+      $template_name = $code . "-extended.html";
+      break;
+	default:
+      log_message ("prepare_card.switch_format", "error");	
+      exit;	  
+  }	  
 
   // Die HTML-Vorlage reinen Text vom Linux-Server einlesen
   $html_template = file_get_contents ($template_name);
@@ -336,17 +480,29 @@ function create_mapping ($file_path)
 		
         $str_name     = get_hash_name ($str_line);  
         $str_name_ext = "{{" . $str_name . "}}";         	  
-	$str_value    = get_hash_value ($handle); // ret: str/false.
+	    $str_value    = get_hash_value ($handle); // ret: str/false.
 		
-	if ($str_value === false) {
-	  log_message ("create_mapping.hash-value", "error");
-	  return false;
-	}
+	    if ($str_value === false) {
+	      log_message ("create_mapping.hash-value", "error");
+	      return false;
+	    }
 		
         $arr_ret_value [$str_name_ext] = $str_value;
 		
-	log_message ("Hash: {$str_name} {$str_value}", "info");		
+	    log_message ("Hash: {$str_name} {$str_value}", "info");				
       }
+	  else {
+        // evidence#array-start
+        $bool_result = str_check_array ($str_line); // ret.: bool.
+        if ($bool_result === true) {
+          log_message ("Array start", "info");				  
+		  
+          $str_name     = get_array_name ($str_line);  
+          $str_name_ext = "{{" . $str_name . "}}";         	  
+	      $str_value    = get_array_value ($handle); // ret: str/false.  
+		  $arr_ret_value [$str_name_ext] = $str_value;		  
+		}		  
+	  }
     }			
 	
   } // while	  
