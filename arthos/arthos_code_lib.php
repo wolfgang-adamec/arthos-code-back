c6d33914125d4ffc7ef8ff3a48a4e0748dbb4f22
Wolfgang Adamec <wolfgang.adamec@gmail.com>
2026-07-18 15:56:07 +0200

    next

--- arthos/arthos_code_lib.php
+++ arthos/arthos_code_lib.php
@@ -1,7 +1,7 @@
 <?php
 
 /*******************************************************************************
- * Dokument: arthos_code_lib.php.                                              *
+ * Dokument: artos_code_lib.php.                                               *
  * Author:   Wolfgang Adamec.                                                  *
  * Date:     2026-06-26.                                                       *
  * License:  Apache 2.0.                                                       *
@@ -25,7 +25,9 @@ function log_message ($message, $level)
 }
 
 /*******************************************************************************
- *                                                                             *
+ * In dieser Funktion wird aus mehreren Dateinamen derjenige herausgesucht,    *
+ * der an einer bestimmten Stelle im Namen einen Buchstaben hat und keine      *
+ * Ziffer.                                                                     *
  *******************************************************************************/
 function find_entry ($arr_files, $int_name_len)
 {  
@@ -33,6 +35,9 @@ function find_entry ($arr_files, $int_name_len)
   $bool_digit    = false;
   $str_ret_value = "";
   $str_file_name = "";
+  
+  // lexeme_deu_1_3_1_actions_context_dialog.art, lexeme_deu_1_3_1_1_turm.
+  // lexeme_deu_1_3_1_  
   
   foreach ($arr_files as $str_path) {
 	log_message ("find_entry_file", "info");	
@@ -43,6 +48,7 @@ function find_entry ($arr_files, $int_name_len)
     $char_char     = $str_file_name [$int_name_len];      	  
 	log_message ($int_name_len, "info");	
 	
+	// Prueft, ob alle Zeichen eines Strings Ziffern sind.
 	$bool_digit = ctype_digit ($char_char);
     if ($bool_digit === false) {
       $str_ret_value = $str_file_name;
@@ -55,38 +61,43 @@ function find_entry ($arr_files, $int_name_len)
 
 /*******************************************************************************
  *                                                                             *
+ * Input parameters:                                                           *
+ * 1) str_base_dir                                                             *
+ * 2) str_name_part                                                            *
+ *                                                                             *
+ * Return value: str_file_name | false.                                        *                                                                                        
  *******************************************************************************/
-function get_full_file_name ($base_dir, $name_part)
+function get_full_file_name ($str_base_dir, $str_name_part)
 {
-  $arr_dateien   = [];
-  $suchmuster    = "";
-  $int_count     = 0;
-  $file_name     = "";
-  $file_path     = "";
-  $name_part_len = 0;
+  $arr_dateien       = [];
+  $str_suchmuster    = "";
+  $int_count         = 0;
+  $str_file_name     = "";
+  $str_file_path     = "";
+  $int_name_part_len = 0;
 
   // lexeme_deu_1_3_1_actions_context_dialog.art, lexeme_deu_1_3_1_1_turm.
   // lexeme_deu_1_3_1_
 
-  $name_part_len = strlen ($name_part);
-  $suchmuster    = $base_dir . DIRECTORY_SEPARATOR . $name_part . "*";
+  $int_name_part_len = strlen ($name_part);
+  $str_suchmuster    = $str_base_dir . DIRECTORY_SEPARATOR . $str_name_part . "*";
 
-  $arr_dateien = glob ($suchmuster);
+  $arr_dateien = glob ($str_suchmuster);
   if ($arr_dateien === false) {
     return false;
   }
 
   $int_count = count ($arr_dateien);
   if ($int_count === 1) {
-    $file_path = $arr_dateien [0];
+    $str_file_path = $arr_dateien [0];
     // Ich bekomme einen ganzen Pfad zurueck, moechte aber nur den Dateinamen zurueckgeben.
-    $file_name = basename ($file_path);
+    $str_file_name = basename ($str_file_path);
   }
   else {
-	$file_name = find_entry ($arr_dateien, $name_part_len);
+	$str_file_name = find_entry ($arr_dateien, $int_name_part_len);
   }
 
-  return $file_name;
+  return $str_file_name;
 }
 
 /*******************************************************************************
@@ -263,12 +274,18 @@ function get_array_value ($handle)
 }
 
 /*******************************************************************************
- *                                                                             *                                                                       
+ * This function processes the whole associative array with the input          *                                                                       
+ * parameters.                                                                 *
+ *
+ * Input parameters:                                                           *
+ * 1) arr_query_string                                                         *
+ *                                                                             *
+ * Return value: Mapping (Ass. Array) | false.                                 *                                                                                     
  *******************************************************************************/
 function query_str_get_value ($arr_query_str, $str_attr)
 {
   $bool_result = false;
-  $str_return  = "";  
+  $str_return  = "";         
    
   $bool_result = array_key_exists ($str_attr, $arr_query_str);
   
@@ -282,80 +299,228 @@ function query_str_get_value ($arr_query_str, $str_attr)
 }
 
 /*******************************************************************************
- *                                                                             *                                                                                       
+ * This function processes the whole associative array with the input          *                                                                       
+ * parameters.                                                                 *
+ *
+ * Input parameters:                                                           *
+ * 1) arr_query_string                                                         *
+ *                                                                             *
+ * Return value: Ass. Array | false.                                           *                                                                                     
  *******************************************************************************/
-function log_mapping ($mapping)
+function scan_query_string ($arr_query_str)
 {
-  $log_line = "";
+  $arr_result      = [];
+  $int_anzahl      = 0;
+  $int_dummy       = 0;
+  $bool_id_set     = false;
+  $bool_lang_set   = false;
+  $bool_format_set = false;  
   
-  foreach ($mapping as $name => $value) {
-    $log_line .= "{$name}: {$value}\n";	  
+  $int_anzahl = count ($arr_query_str);
+  if (($int_anzahl !== 2) && ($int_anzahl !== 3)) {
+    log_message ("scan_query_str.error-count", "error");
+    return false;	  
   }
-  log_message ($log_line, "info");
+
+  foreach ($arr_query_str as $str_key => $str_value) {
+	$bool_result = check_array_part ($str_key, $str_value);  
+    if ($bool_result === false) {
+      log_message ("scan_query_str.error-count", "error");
+      return false;
+    }
+	
+	$arr_result [$str_key] = $str_value;
+	
+	if ($str_key === "id") {
+	  $bool_id_set = true;
+	}
+	if ($str_key === "lang") {
+	  $bool_lang_set = true;
+	}
+	if ($str_key === "format") {
+	  $bool_format_set = true;
+	}	
+  }  
+  
+  if (($bool_id_set   === true) &&
+	  ($bool_lang_set === true)) {
+    $int_dummy = 0;		  
+  }		  
+  else {
+    log_message ("scan_query_str.err-not-all", "error");
+    break;	  	  
+  }	  
+
+  if ($bool_format_set === false) {
+	$arr_result ["format"] = "normal";  
+  }
+  
+  return $arr_result;
 }
 
 /*******************************************************************************
- * Return: Mapping (Ass. Array) | false.                                       *                                                                                       
+ * This function checks if the key and the value have a certain structure.     *
+ *                                                                             *
+ * Input parameters:                                                           *
+ * 1) str_key                                                                  *
+ * 2) str_value                                                                *
+ *                                                                             *
+ * Return values: true | false.                                                *                                                                                     
  *******************************************************************************/
-function prepare_card ($code, $base_dir)
+function check_array_part ($str_key, $str_value)
 {
-  $str_id         = "";
-  $str_lang       = ""; 
-  $str_format     = "";
-  $str_sapi_name  = "";
-  $file_name_full = "";
-  $file_name_part = "";
-  $html_template  = "";
-  $output         = "";
-  $template_name  = "";
+  $int_value_len = 0;
+
+  $int_value_len = strlen ($str_value);
+  
+  if ($int_value_len === 0) {
+	log_message ("check_array_part.error-len-0", "error");
+	return false;
+  }
+
+  if (($str_key !== "id"    ) &&
+      ($str_key !== "lang"  ) &&
+      ($str_key !== "format")) {
+	log_message ("check_array_part.err-key", "error");
+	return false;		  
+  }
+
+  if ($str_key === "id") {
+    if ($int_value_len === 0) {
+	  log_message ("check_array_part.err-len-id", "error");
+	  return false;		
+    }
+  }
+  
+  if ($str_key === "lang") {
+    if ($int_value_len !== 3) {
+	  log_message ("check_array_part.err-len-lang", "error");
+	  return false;
+    }  
+  }
+  
+  if ($str_key === "format") {
+    if (($str_value !== "normal") &&
+        ($str_value !== "short" ) &&
+		($str_value !== "ext"   )) {
+	  log_message ("check_array_part.err-format-value", "error");
+	  return false;  
+    }
+  }
+
+  return true;  
+}
+
+/*******************************************************************************
+ * In dieser Funktion wird der Inhalt eines assoziativen Arrays ausgedruckt.   *                                                                                       
+ *******************************************************************************/
+function log_mapping ($arr_mapping)
+{
+  $str_log_line = "";
+  
+  foreach ($arr_mapping as $str_name => $str_value) {
+    $str_log_line .= "{$str_name}: {$str_value}\n";	  
+  }
+  log_message ($str_log_line, "info");
+}
+
+/*******************************************************************************
+ * This function checks if a string starts with a certain substring.           *
+ *                                                                             *
+ * Input parameters:                                                           *
+ * 1) str_value.                                                               *
+ * 2) str_substr.                                                              *
+ *                                                                             *
+ * Return value: true | false.                                                 *                                                                                         
+ *******************************************************************************/
+function artos_str_starts_with ($str_par_value, $str_par_substr)
+{
+  $str_substr     = "";
+  $int_len_substr = 0;
+  
+  $int_len_substr = strlen (str_par_substr);
+  $str_substr     = substr (str_par_value, 0, $int_len_substr - 1);
+  
+  if ($str_par_substr === $str_substr) {
+	return true;
+  }
+  else {
+	return false;  
+  }	  
+
+}
+
+/*******************************************************************************
+ * This is the function, that is called by "show-person.php" and               *
+ * "show-lexeme.php".                                                          *
+ * It does the following things:                                               *
+ * - The parameters of the query string are being fetched.                     *
+ *                                                                             *
+ * Input parameters:                                                           *
+ * 1) str_code: For example "lexeme" or "person". It is used to build names.   *
+ *                                                                             *
+ * Return value: Mapping (Ass. Array) | false.                                 *                                                                                       
+ *******************************************************************************/
+function prepare_card ($str_code, $str_base_dir)
+{
+  $str_par_id         = "";
+  $str_par_lang       = "";
+  $str_par_format     = "";
+  $str_sapi_name      = "";
+  $str_file_name_full = "";
+  $str_file_name_part = "";
+  $str_template_name  = "";
+  $str_html_template  = "";
+  $str_output         = "";
+  $arr_input_params   = [];
+  $arr_data_map       = [];
 
   $str_sapi_name = php_sapi_name ();
   if ($str_sapi_name === "cli") {
+	// If the script is being called from the the command line interface, these default input parameter values are assigned.
     $_GET["id"]     = "1.1";
     $_GET["lang"]   = "deu";
 	$_GET["format"] = "normal";
   }
 
-  $str_id = query_str_get_value ($_GET, "id"); // false/str.  
-  if ($str_id === false) {
-    log_message ("prepare_card.query_str_get_value-id", "error");	
-    exit;
-  } 
+  $arr_input_params = scan_query_string ($_GET);
+  if ($arr_input_params === false) {
+    log_message ("prepare_card.scan_query_string", "error");	
+    return false;	  
+  }	  
 
-  $str_lang = query_str_get_value ($_GET, "lang"); // false/str.
-  if ($str_lang === false) {
-    log_message ("prepare_card.query_str_get_value-lang", "error");
-    exit;
-  }  
+  // In this section, one by one, the 3 query string parameters are fetched.
+  // "id" and "lang" are mandatory input parameters, "format" is optional.
 
-  $str_format = query_str_get_value ($_GET, "format"); // false/str.  
-  if ($str_format === false) {
-    $str_format = "normal";  
-    log_message ("prepare_card.format.default", "info");
-  } 
+  $str_par_id     = $arr_input_params ["id"];
+  $str_par_lang   = $arr_input_params ["lang"];
+  $str_par_format = $arr_input_params ["format"];  
 
-  log_message ("ID:     {$str_id}",     "info");
-  log_message ("lang:   {$str_lang}",   "info");
-  log_message ("format: {$str_format}", "info");
+  log_message ("ID:     {$str_par_id}",     "info");
+  log_message ("lang:   {$str_par_lang}",   "info");
+  log_message ("format: {$str_par_format}", "info");
 
-  $str_id         = str_replace (".", "_", $str_id);  
-  $file_name_part = $code . "_" . $str_lang . "_" . $str_id . "_";
+  // lexeme_deu_1_4_1_weg.art
+
+  $str_par_id         = str_replace (".", "_", $str_par_id);  
+  $str_file_name_part = $str_code . "_" . $str_par_lang . "_" . $str_par_id . "_";
   
-  log_message ($file_name_part, "info");
+  log_message ($str_file_name_part, "info");
   
-  $file_name_full = get_full_file_name ($base_dir, $file_name_part);
-  if ($file_name_full ===  false) {
+  $str_file_name_full = get_full_file_name ($str_base_dir, $str_file_name_part);
+  if ($str_file_name_full === false) {
     log_message ("prepare_card.get_full_file_name", "error");
-    exit;
+    return false;
   }
 
-  log_message ($file_name_full, "info");
+  log_message ($str_file_name_full, "info");
 
-  $data_map = create_mapping ($file_name_full); 
-  log_mapping ($data_map);  
-  if ($data_map === false) {
+  // Aus dem Inhalt der ART-Datei wird fuer das HTML-Template ein Mapping erstellt.
+  $arr_data_map = create_mapping ($str_file_name_full); 
+  log_mapping ($arr_data_map);  
+  if ($arr_data_map === false) {
     log_message ("prepare_card.create_mapping", "error");	
-    exit;
+    return false;
   }	  
   
   log_message ("nach create_mapping", "info");
@@ -363,13 +528,13 @@ function prepare_card ($code, $base_dir)
   // lexeme-template.html, lexeme-short.html, lexeme-extended.html
   switch ($str_format) {
     case "normal": 
-      $template_name = $code . "-template.html";
+      $str_template_name = $str_code . "-template.html";
       break;
     case "short":
-      $template_name = $code . "-short.html";
+      $str_template_name = $str_code . "-short.html";
       break;
     case "ext":
-      $template_name = $code . "-extended.html";
+      $str_template_name = $str_code . "-extended.html";
       break;
     default:
       log_message ("prepare_card.switch_format", "error");	
@@ -377,18 +542,21 @@ function prepare_card ($code, $base_dir)
   }	  
 
   // Die HTML-Vorlage reinen Text vom Linux-Server einlesen
-  $html_template = file_get_contents ($template_name);
+  $str_html_template = file_get_contents ($str_template_name);
 
-  // Platzhalter durch echte Daten ersetzen
-  $output = strtr ($html_template, $data_map);
+  // Platzhalter durch echte Daten ersetzen.
+  $str_output = strtr ($str_html_template, $arr_data_map);
  
-  return $output; 
+  return $str_output; 
 }
 
 /*******************************************************************************
- * Return: Mapping (Ass. Array) | false.                                       *                                                                                       
+ * In dieser Funktion wird aus dem Inhalt einer ART-Datei fuer das HTML-       *
+ * Template ein Mapping erstellt.                                              *
+ *                                                                             *
+ * Return value: Mapping (Ass. Array) | false.                                 *                                                                                       
  *******************************************************************************/
-function create_mapping ($file_path)
+function create_mapping ($str_file_path)
 {
   $arr_parts     = [];
   $arr_ret_value = [];
@@ -409,7 +577,7 @@ function create_mapping ($file_path)
     return false;	
   }	  
   
-  $handle = fopen ($file_path, "r"); // ret.: resource/false.
+  $handle = fopen ($str_file_path, "r"); // ret.: resource/false.
   
   if ($handle === false) { 
     log_message ("create_mapping.fopen", "error");
